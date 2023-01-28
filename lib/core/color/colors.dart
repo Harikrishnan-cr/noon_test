e645ce9d70a4ae388f9b4d933a759e7af09dfa37
Harikrishnan-cr <harikrishnancr1725@gmail.com>
2023-01-28 14:33:03 +0530

    folder structure completed

---
+++ lib/core/color/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const whiteColor = Colors.white;
+  static const blackColor = Colors.black;
+  static const greyColor = Colors.grey;
+  static const amberColor = Colors.amber;
+  static const blueColor = Color.fromARGB(255, 18, 95, 182);
+  static const lightYellow = Color.fromARGB(255, 255, 212, 73);
+}
